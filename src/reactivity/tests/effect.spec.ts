3d3b2a21ad2a6505279c08a144329ef1e62a4115
Step_by_step <40707695+threeStepWalk@users.noreply.github.com>
2022-04-18 21:12:56 +0800

    feat: scheduler

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -19,6 +19,8 @@ describe('effect', () => {
     user.age++
     expect(nextAge).toBe(12)
   })
+
+  // 返回runner函数
   it('should return runner when call effect', () => {
     // 1. effect(fn) -> function(runner) -> fn -> return 
     let foo = 10
@@ -33,5 +35,31 @@ describe('effect', () => {
 
     expect(foo).toBe(12)
     expect(r).toBe("foo")
+  })
+
+  it("scheduler", () => {
+    // 1. 通过 effect 的第二个参数给定的一个 scheduler 的 fn
+    // 2. effect 第一次执行时，还会执行 fn
+    // 3. 当响应式对象 set update 不会执行 fn， 而是执行 scheduler
+    // 4. 如果说当前执行 runner 时， 会再次执行 fn
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+    run()
+    expect(dummy).toBe(2)
   })
 })
