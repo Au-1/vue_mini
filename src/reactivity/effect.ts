f5fe4a0926df3077aae51b1d33d23a20874c05cd
Step_by_step <40707695+threeStepWalk@users.noreply.github.com>
2022-04-19 20:56:36 +0800

    effect-stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,10 @@
+import { extend } from "../shared"
+
 class ReactiveEffect {
   private _fn: any
-
+  deps = []
+  active = true
+  onStop?: () => void
   constructor(fn, public scheduler?) {
     this._fn = fn
   }
@@ -8,6 +12,21 @@ class ReactiveEffect {
     activeEffect = this
     return this._fn()
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  });
 }
 
 const targetMap = new Map()
@@ -20,6 +39,8 @@ export function track(target, key) {
     targetMap.set(target, depsMap)
   }
 
+  if (!activeEffect) return
+
   let dep = depsMap.get(key)
 
   if (!dep) {
@@ -28,6 +49,7 @@ export function track(target, key) {
   }
 
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function triggle(target, key) {
@@ -46,10 +68,21 @@ export function triggle(target, key) {
 
 let activeEffect
 export function effect(fn, options: any = {}) {
-
+  // fn
   const _effect = new ReactiveEffect(fn, options.scheduler)
+
+  Object.assign(_effect, options)
+  // extend 
+  extend(_effect, options)
 
   _effect.run()
 
-  return _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
 }
+
+export function stop(runner) {
+  runner.effect.stop()
+} 
