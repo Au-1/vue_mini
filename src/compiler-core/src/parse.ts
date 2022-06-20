392ca67b5a094b1f2ec3666d506abe1aa02c01cc
Step_by_step <40707695+threeStepWalk@users.noreply.github.com>
2022-06-20 22:06:40 +0800

    compiler-core: 解析element标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast"
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -10,8 +15,14 @@ function parseChildren(context) {
   const nodes: any = []
 
   let node;
-  if (context.source.startsWith("{{")) {
+  const s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+
+    }
   }
 
   nodes.push(node)
@@ -62,5 +73,31 @@ function createRoot(children) {
 function createParserContext(content: string) {
   return {
     source: content
+  }
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+  console.log("---------", context.source);
+
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+  // 1. 解析 tag
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+
+  // 2. 删除处理完的代码
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
   }
 }
